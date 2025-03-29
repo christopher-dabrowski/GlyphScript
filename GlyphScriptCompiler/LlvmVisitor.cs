1a213126641f18e0ff43f4c905f226e98cee8ab7
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-03-29 17:54:09 +0100

    Generate and configure C# project structure

---
+++ GlyphScriptCompiler/LlvmVisitor.cs
@@ -0,0 +1,6 @@
+namespace GlyphScriptCompiler;
+
+public class LlvmVisitor : GlyphScriptBaseVisitor<object>
+{
+
+}
---
+++ GlyphScriptCompiler/Program.cs
@@ -0,0 +1,15 @@
+﻿using Antlr4.Runtime;
+using GlyphScriptCompiler;
+
+var input = "";
+
+var inputStream = new AntlrInputStream(input.ToString());
+var lexer = new GlyphScriptLexer(inputStream);
+var tokenStream = new CommonTokenStream(lexer);
+var parser = new GlyphScriptParser(tokenStream);
+
+var context = parser.program();
+var visitor = new LlvmVisitor();
+visitor.Visit(context);
+
+
