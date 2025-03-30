15c1c687d0f509f8be40e4e247e4b2dc9b63c372
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-03-30 12:24:32 +0200

    Process file passed from the args

--- GlyphScriptCompiler/Program.cs
+++ GlyphScriptCompiler/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Antlr4.Runtime;
 using GlyphScriptCompiler;
 
-var input = "";
+var codeFilePath = args.FirstOrDefault();
+var codeFile = OpenCodeFile(codeFilePath);
+if (codeFile == null)
+    return 1;
 
-var inputStream = new AntlrInputStream(input.ToString());
+var inputStream = new AntlrInputStream(codeFile);
 var lexer = new GlyphScriptLexer(inputStream);
 var tokenStream = new CommonTokenStream(lexer);
 var parser = new GlyphScriptParser(tokenStream);
@@ -12,4 +15,20 @@ var context = parser.program();
 var visitor = new LlvmVisitor();
 visitor.Visit(context);
 
+return 0;
 
+Stream? OpenCodeFile(string? codeFilePath)
+{
+    if (codeFilePath is null)
+    {
+        Console.Error.WriteLine("No code file specified. Pass a file name to run.");
+        return null;
+    }
+    if (!File.Exists(codeFilePath))
+    {
+        Console.Error.WriteLine($"File {codeFilePath} does not exist.");
+        return null;
+    }
+
+    return File.OpenRead(codeFilePath);
+}
