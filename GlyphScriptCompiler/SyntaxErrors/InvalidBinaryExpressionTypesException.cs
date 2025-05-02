d906c4ac18ed09664eb07d94de710bc82a263ec8
Krzysztof Dąbrowski <19330758+christopher-dabrowski@users.noreply.github.com>
2025-05-02 13:24:12 +0200

    Fix warning

--- GlyphScriptCompiler/SyntaxErrors/InvalidBinaryExpressionTypesException.cs
+++ GlyphScriptCompiler/SyntaxErrors/InvalidBinaryExpressionTypesException.cs
@@ -4,8 +4,9 @@ namespace GlyphScriptCompiler.SyntaxErrors;
 
 public class InvalidBinaryExpressionTypesException(ParserRuleContext context) : InvalidSyntaxException(context)
 {
+    private readonly ParserRuleContext _context = context;
     public required GlyphScriptType FirsType { get; init; }
     public required GlyphScriptType SecondType { get; init; }
 
-    public override string Reason => $"Types {FirsType} and {SecondType} are not valid for the {context.GetText()}";
+    public override string Reason => $"Types {FirsType} and {SecondType} are not valid for the {_context.GetText()}";
 }
